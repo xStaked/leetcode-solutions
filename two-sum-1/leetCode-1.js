8fdad90f7145dbc71653c43224b8f05e81715292
= <=>
2023-05-02 21:52:43 -0500

    add folder and typescript solution

---
+++ two-sum-1/leetCode-1.js
@@ -0,0 +1,14 @@
+const twoSum = (nums, target) => {
+  const len = nums.length;
+  const result = [];
+
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (nums[i] + nums[j] == target) {
+        result.push(i, j);
+      }
+    }
+  }
+
+  return result;
+};
--- two-sum-1/leetCode-1.ts
+++ two-sum-1/leetCode-1.ts
@@ -1,6 +1,6 @@
-const twoSum = () => {
-  const len = nums.length;
-  const result = [];
+const twoSumTs = (nums: number[], target: number): number[] => {
+  const len: number = nums.length;
+  const result: number[] = [];
 
   for (let i = 0; i < len; i++) {
     for (let j = i + 1; j < len; j++) {
